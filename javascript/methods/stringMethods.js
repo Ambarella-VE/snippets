3cd6e7849d6fcc75cb234e38d5cd5d0eb9681f04
Ambar Canonicco <64762162+Ambarella-VE@users.noreply.github.com>
2021-08-27 23:21:14 -0300

    add toProperCase js extended method

---
+++ javascript/methods/stringMethods.js
@@ -0,0 +1,15 @@
+/* -------------------------------------------- */
+/*                  Title Case                  */
+/* -------------------------------------------- */
+
+String.prototype.toProperCase = function(str) {
+  return (str ? this.toString().toLowerCase() : this).replace(
+    /\w\S*/g, txt => {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  )
+}
+
+
+
+console.log("ambar caNonicco".toProperCase())
